ab147990eb97be81fa16c12c9b8bda46497e033b
Alexander <v0xt.alex@gmail.com>
2021-12-29 13:00:02 +0300

    contracts: rm hardhat.console import

--- contracts/DAO.sol
+++ contracts/DAO.sol
@@ -2,7 +2,6 @@
 
 pragma solidity ^0.8.10;
 
-import "hardhat/console.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
